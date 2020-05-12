50133b3337b580a7b93f5b1d8c12ef01fb72d6e4
Muhamar <amararrasyid@gmail.com>
2020-05-12 18:43:20 +0700

    Tugas 28 Javascript
    Mengumpulkan Tugas Niomic

---
+++ index.js
@@ -0,0 +1,9 @@
+function regex() {
+    let str = "abcdefghijklmnopqrstuvwxyz";
+    let s = str.indexOf("s");
+    var tampil = "Urut ke : ";
+
+    var result = tampil + s;
+    console.log(result);
+}
+regex()
